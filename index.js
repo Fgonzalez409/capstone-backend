f26d562aa7f6aefe459313a71275b201dc1e1a22
Fgonzalez409 <fergonz409@gmail.com>
2023-09-23 16:51:39 -0500

    test

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(function (req, res, next) {
 
 
 //******************************************************************************************************************************************************* */
-    res.setHeader("Access-Control-Allow-Origin", "https://mellifluous-sfogliatella-b5b761.netlify.app/");
+    res.setHeader("Access-Control-Allow-Origin", "https://capstone-backend-blush.vercel.app");
 //*****************************************************************************************************************************************************
 
     // res.setHeader(
