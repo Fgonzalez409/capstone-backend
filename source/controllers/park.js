f6f612dce93ad649451f57af8c53cb08533821ac
Fgonzalez409 <fergonz409@gmail.com>
2023-10-01 17:46:47 -0500

    fixed park route and included middleware

--- source/controllers/park.js
+++ source/controllers/park.js
@@ -2,15 +2,16 @@ const pool = require("../sql/connection")
 
 
 const park = (req,res) => {
-    const{id, park_id, user_id} = req.body
-    pool.query(`INSERT INTO visitedParks (id, park_id, user_id) VALUES (?, ?, ?)`, 
-    [id, park_id, user_id],
+    const{park_id} = req.body
+    const {user_id} = req.user
+    pool.query(`INSERT INTO visitedParks ( park_id, user_id) VALUES ( ?, ?)`, 
+    [park_id, user_id],
     (err, rows, fields) => {
         if(err) {
             return res.status(500).json({message: err.message})
         }
 
-        res.json({visitedParks})
+        res.json(rows)
     })
 }
 
--- source/routes/park.js
+++ source/routes/park.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const router = express.Router()
 const parkControllers = require("../controllers/park")
+const {authenticateJWT} = require("../auth")
 // const auth = require("../auth")
 
-router.post("/park", parkControllers.park )
+router.post("/park", authenticateJWT, parkControllers.park )
 // router.post("/signin", auth.authenticateJWT, signinControllers.signin )
 
 module.exports = router
