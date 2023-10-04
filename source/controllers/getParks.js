28c93f3dcf2f26c231368271d68cbe0f82409f63
Fgonzalez409 <fergonz409@gmail.com>
2023-10-03 21:54:55 -0500

    error retrieving parks, circular reference

--- source/controllers/getParks.js
+++ source/controllers/getParks.js
@@ -2,13 +2,20 @@ const pool = require("../sql/connection")
 
 
 
-const list = (req, res) => {console.log(pool)
-    const {user_id} = req.body
-    pool.query(`SELECT * FROM visitedPark WHERE user_id = ?`, 
-    [user_id],
-    (err, rows, fields) => {
-        res.json(rows)
+const list = (user_id) => {
 
+    return new Promise((resolve, reject) => {
+        const query = "SELECT * FROM visitedParks where user_id = ?"
+
+        pool.query(query,[user_id], (err, results) => {
+            if(err){
+                console.error("Error retrieving park", err)
+                reject(err)
+            }else {
+                console.log("Parks retrieved successfully", results)
+                resolve(results)
+            }
+        })
     })
 }
 
--- source/routes/getParks.js
+++ source/routes/getParks.js
@@ -2,6 +2,15 @@ const express = require("express")
 const router = express.Router()
 const getParksControllers = require("../controllers/getParks")
 
-router.get("/getParks", usersControllers.list )
+router.get("/getParks", async(req, res)=> {
+    const user_id = req.user_id
+    try {
+        const parks = await getParksControllers.list(user_id)
+        res.json(parks)
+    } catch {
+        console.error("error retrieving park", error)
+        res.status(500).json({error: "Server Error"})
+    }
+})
 
 module.exports = router
