d5c147e05e29d7f783d283423d756723e929c4b8
Fgonzalez409 <fergonz409@gmail.com>
2023-10-31 19:14:40 -0500

    test

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(function (req, res, next) {
 
 
 //******************************************************************************************************************************************************* */
-    res.setHeader("Access-Control-Allow-Origin", "https://mellifluous-sfogliatella-b5b761.netlify.app/signin");
+    res.setHeader("Access-Control-Allow-Origin", "https://mellifluous-sfogliatella-b5b761.netlify.app/");
 //*****************************************************************************************************************************************************
   
     // Request methods you wish to allow
--- source/routes/getSavedParks.js
+++ source/routes/getSavedParks.js
@@ -6,16 +6,3 @@ const {authenticateJWT} = require("../auth")
 router.get("/getSavedParks", getSavedParksControllers.getSavedParks)
 
 module.exports = router
-
-
-// , authenticateJWT, async(req, res)=> {
-//     // const userID = req.user.id
-//     // console.log(userID)
-//     // try {
-//     //     const parks = await getSavedParksControllers.list(userID)
-//     //     res.json(parks)
-//     // } catch {
-//     //     console.error("error retrieving park", error)
-//     //     res.status(500).json({error: "Server Error"})
-//     // }
-// }
