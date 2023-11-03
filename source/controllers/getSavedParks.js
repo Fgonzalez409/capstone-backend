4ff0b3fce12b4520b5300f017dcd57474bc1d3d9
Fgonzalez409 <fergonz409@gmail.com>
2023-11-02 20:24:17 -0500

    header fix

--- source/controllers/getSavedParks.js
+++ source/controllers/getSavedParks.js
@@ -5,19 +5,20 @@ const pool = require("../sql/connection")
 const getSavedParks = (req,res) => {
     const userID = req.user.id
 
-    return new Promise((resolve, reject) => {
+    // return new Promise((resolve, reject) => {
         const query = "SELECT * FROM visitedParks where user_id = ?"
 
         pool.query(query,[userID], (err, results) => {
             if(err){
                 console.error("Error retrieving park", err)
-                reject(err)
+                // reject(err)
+                res.sendStatus()
             }else {
                 console.log("Parks retrieved successfully", results)
-                resolve(results)
+                res.json(results)
             }
         })
-    })
+    // })
 }
 
 module.exports = {
