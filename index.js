9a0180f878ccb0d0f8f192d2b6404d88e06fbf41
Fgonzalez409 <fergonz409@gmail.com>
2023-11-02 19:48:59 -0500

    fixed header methods

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(function (req, res, next) {
 //*****************************************************************************************************************************************************
   
     // Request methods you wish to allow
-    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Methods", "POST,GET,PUT,DELETE");
   
     // Request headers you wish to allow
     res.setHeader(
