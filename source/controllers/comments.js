f37c73bd26cf8e971af6f06618db54dcf0bcba21
Fgonzalez409 <fergonz409@gmail.com>
2023-11-11 11:45:26 -0600

    updated comments controller

--- source/controllers/comments.js
+++ source/controllers/comments.js
@@ -3,10 +3,11 @@ const pool = require("../sql/connection")
 //save park to DB
 const comments = (req,res) => {
     const{parkCode} = req.body
+    const{comment} = req.body
     console.log(req.user)
     const {id: user_id} = req.user
-    pool.query(`INSERT INTO comments (park_id_comment, comment) VALUES ( ?, ?)`, 
-    [parkCode, user_id],
+    pool.query(`INSERT INTO comments (parkCodeComment, comment) VALUES ( ?, ?)`, 
+    [parkCode, comment],
     (err, rows, fields) => {
         if(err) {
             return res.status(500).json({message: err.message})
