186dd1035ea18784c4cb057ac9f368916471fb68
Fgonzalez409 <fergonz409@gmail.com>
2023-11-10 18:22:07 -0600

    test

---
+++ source/routes/comments.js
@@ -0,0 +1,8 @@
+// const express = require("express")
+// const router = express.Router()
+// const commentsControllers = require("../controllers/comments")
+// const {authenticateJWT} = require("../auth")
+
+// router.post("/comments", authenticateJWT, commentsControllers.comments )
+
+// module.exports = router
