a30ce2d7b2aaa7cf6ff282a1426702376b88348c
Fgonzalez409 <fergonz409@gmail.com>
2024-02-13 13:39:50 -0600

    added getComments route and getComments controller

--- source/controllers/getSavedParks.js
+++ source/controllers/getSavedParks.js
@@ -4,10 +4,7 @@ const pool = require("../sql/connection")
 
 const getSavedParks = (req,res) => {
     const userID = req.user.id
-
-    // return new Promise((resolve, reject) => {
         const query = "SELECT * FROM visitedParks where user_id = ?"
-
         pool.query(query,[userID], (err, results) => {
             if(err){
                 console.error("Error retrieving park", err)
@@ -18,7 +15,6 @@ const getSavedParks = (req,res) => {
                 res.json(results)
             }
         })
-    // })
 }
 
 module.exports = {
